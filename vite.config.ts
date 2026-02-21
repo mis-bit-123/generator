ead2b8fc8ecd399dc580e8d14287439a0b85bf6d
mis-bit-123 <mis@coninfra.in>
2026-02-21 15:09:24 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
+import { inspectAttr } from "kimi-plugin-inspect-react"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [inspectAttr(), react()],
+ base: "/generator/",
+  plugins: [
+    inspectAttr(),
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
